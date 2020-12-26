957c7da4a7d5f8a42cfb7ee980e94fcbee9008ce
luca <luca.vidali@gmail.com>
2020-12-26 23:02:16 +0100

    First using

--- luca.py
+++ luca.py
@@ -1,1 +1,11 @@
-print ("hello world!")
+import ipaddress
+import math
+for ip in ipaddress.ip_network('192.168.100.0/23'):
+    print("your ip address is:", ip)
+val = input ("Tell me how do you wanna name this network: ")
+print (val)
+radice = 2**3
+print (radice)
+
+
+
